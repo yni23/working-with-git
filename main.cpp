510c55758eef4ccf0f8aec141ab2b2438eb8c824
Michael H. Scott <michael.scott@oregonstate.edu>
2023-02-20 14:21:00 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+const char* Akiri(void) {
+  return "/*Type something here*/";
+}
+
+const char* Barb(void) {
+  return "/*Type something here*/";
+}
+
+const char* Bryam(void) {
+  return "/*Type something here*/";
+}
+
+const char* Gabe(void) {
+  return "/*Type something here*/";
+}
+
+const char* Gustavo(void) {
+  return "/*Type something here*/";
+}
+
+const char* Jacob(void) {
+  return "/*Type something here*/";
+}
+
+const char* Michael(void) {
+  return "/*Type something here*/";
+}
+
+const char* Minjie(void) {
+  return "/*Type something here*/";
+}
+
+const char* Nick(void) {
+  return "/*Type something here*/";
+}
+
+const char* Sebastian(void) {
+  return "/*Type something here*/";
+}
+
+const char* Yun(void) {
+  return "/*Type something here*/";
+}
+
+const char* Zahra(void) {
+  return "/*Type something here*/";
+}
+
+const char* Zarak(void) {
+  return "/*Type something here*/";
+}
+
+
+int main (int argc, const char **argv)
+{
+  cout << "Hello World" << endl << endl;
+
+  cout << "Akiri: " << Akiri() << endl;
+  cout << "Barb: " << Barb() << endl;
+  cout << "Bryam: " << Bryam() << endl;
+  cout << "Gabe: " << Gabe() << endl;
+  cout << "Gustavo: " << Gustavo() << endl;
+  cout << "Jacob: " << Jacob() << endl;
+  cout << "Michael: " << Michael() << endl;
+  cout << "Minjie: " << Minjie() << endl;
+  cout << "Nick: " << Nick() << endl;
+  cout << "Sebastian: " << Sebastian() << endl;
+  cout << "Yun: " << Yun() << endl;
+  cout << "Zahra: " << Zahra() << endl;
+  cout << "Zarak: " << Zarak() << endl;
+  
+  return 0;
+}
